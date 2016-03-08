db3a16b44cc306704153eee11956c3d8e229ffd4
Wilfredo Villegas <wivisisorg@gmail.com>
2016-03-08 00:53:04 -0500

    Demo Functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,36 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+  $scope.user = {pin:'001111', name:'Doe, John', manager:'Doe, Jane', contact: '888-555-444'};
+  $scope.record = {location: 'Operations', signintime:'', signouttime:'', saved: false};
+  $scope.locations = ['Operations','Altmeyer','Annex','WOC','NCC','East Low Rise','East High Rise','West Low Rise','West High Rise'];
+  $scope.myTimes = ['1:00 PM','2:00 PM','3:00 PM','4:00 PM'];
+  $scope.submitLabel = 'Sign In';
+  $scope.submitColor = 'button-balanced';
+  $scope.hideSubmit = false;
+
+  $scope.timesh = function(){
+    return ['1:00 PM','2:00 PM','3:00 PM','4:00 PM'];
+  };
+
+  $scope.signIn = function(){
+    console.log($scope.record);
+
+    if(!$scope.record.saved){
+      $scope.record.saved = true;
+      $scope.submitLabel = 'Sign Out';
+      $scope.submitColor = 'button-assertive';
+    }else{
+      $scope.hideSubmit = true;
+    }
+  };
+
+  $scope.showSignout = function(){
+    return $scope.record.signintime.length > 0 && $scope.record.saved;
+  };
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
